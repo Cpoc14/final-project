254207f0c226b2e2a334de18ff28ee53b1d8a658
CashWasTaken_ <95936719+Cpoc14@users.noreply.github.com>
2021-12-10 14:01:45 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,13 +3,30 @@ input.onButtonPressed(Button.A, function () {
     Lives = 3
     while (true) {
         if (input.pinIsPressed(TouchPin.P0)) {
-        	
+            music.playTone(880, music.beat(BeatFraction.Double))
+            Lives += -1
+            basic.showString("" + (Lives))
         } else if (input.pinIsPressed(TouchPin.P1)) {
         	
         } else if (input.pinIsPressed(TouchPin.P2)) {
         	
         } else {
-        	
+            if (Lives == 0) {
+                music.playTone(262, music.beat(BeatFraction.Breve))
+                basic.showIcon(IconNames.Heart)
+                basic.pause(100)
+                basic.showIcon(IconNames.SmallHeart)
+                basic.pause(100)
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    . . # . .
+                    . . . . .
+                    . . . . .
+                    `)
+                basic.pause(100)
+                break;
+            }
         }
     }
 })
